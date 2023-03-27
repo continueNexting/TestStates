298197b8c6cc9a7a695bd23b3c6c39afc926f597
continueNexting <128931225+continueNexting@users.noreply.github.com>
2023-03-28 08:29:14 +0845

    Update base.cpp

--- src/base.cpp
+++ src/base.cpp
@@ -22,8 +22,8 @@ struct vec3
 };
 inline void MainMenu::update()
 {
-
     Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
+    
     music = Mix_LoadMUS("/home/jimmy/out.mp3");
 
     Mix_VolumeMusic(NULL);
@@ -35,6 +35,9 @@ inline void MainMenu::update()
 
     int fSize = 25;
     TTF_Font *font;
+    std::string kata="ンワラヤマハナタサカアリミヒニチシキイルユムフヌツスクウレメヘネテセケエヲロヨモホノトソコオ";//46
+    std::string hira="かきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょ";//69
+
     std::string texts,texts1;
     SDL_Color foreground;
     SDL_Texture *text,*text1;
@@ -50,102 +53,12 @@ inline void MainMenu::update()
     uint8_t bG = 255;
     uint8_t bB = 255;
     uint8_t bA = 255;
+     
+//     "ンワラヤマハナタサカアリミヒニチシキイルユムフヌツスクウレメヘネテセケエヲロヨモホノトソコオ"
 
-//     "
-//    ン
-//     30F3
-//     ワ
-//     30EF
-//     ラ
-//     30E9
-//     ヤ
-//     30E4
-//     マ
-//     30DE
-//     ハ
-//     30CF
-//     ナ
-//     30CA
-//     タ
-//     30BF
-//     サ
-//     30B5
-//     カ
-//     30AB
-//     ア
-//     30A2
-//     リ
-//     30EA
-//     ミ
-//     30DF
-//     ヒ
-//     30D2
-//     ニ
-//     30CB
-//     チ
-//     30C1
-//     シ
-//     30B7
-//     キ
-//     30AD
-//     イ
-//     30A4
-//     ル
-//     30EB
-//     ユ
-//     30E6
-//     ム
-//     30E0
-//     フ
-//     30D5
-//     ヌ
-//     30CC
-//     ツ
-//     30C4
-//     ス
-//     30B9
-//     ク
-//     30AF
-//     ウ
-//     30A6
-//     レ
-//     30EC
-//     メ
-//     30E1
-//     ヘ
-//     30D8
-//     ネ
-//     30CD
-//     テ
-//     30C6
-//     セ
-//     30BB
-//     ケ
-//     30B1
-//     エ
-//     30A8
-//     ヲ
-//     30F2
-//     ロ
-//     30ED
-//     ヨ
-//     30E8
-//     モ
-//     30E2
-//     ホ
-//     30DB
-//     ノ
-//     30CE
-//     ト
-//     30C8
-//     ソ
-//     30BD
-//     コ
-//     //30B3
-//     オ
-//     //30AA
 
-// "
+
+ //   "かきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょ"
 
     ///usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf
     ///home/jimmy/Downloads/Noto_Sans_KR/NotoSansKR-Light.otf
@@ -161,7 +74,7 @@ inline void MainMenu::update()
 
     std::vector<vec3> point;
     srand(time(NULL));
-    for (int var = 0; var < 3150; ++var)
+    for (int var = 0; var < 4300; ++var)
     {
 
       SDL_Rect rect;
@@ -176,12 +89,11 @@ inline void MainMenu::update()
       rect1.h=29;
       vec3 x;
       x.x = (rand() % widthW - widthW / 2);
-      x.y = 740;
+      x.y = 500;
       x.z = (rand() % widthW - widthW / 2);
       point.push_back(x);
       dest.push_back(rect);
       if(var%7==0)dest.push_back(rect1);
-
     }
     vec3 c{0, 0, 0};
     for (auto &p : point)
@@ -190,29 +102,104 @@ inline void MainMenu::update()
       c.y += p.y;
       c.z += p.z;
     }
-    c.x /= point.size();
-    c.y /= point.size();
-    c.z /= point.size();
+    c.x /= point.size()-1;
+    c.y /= point.size()-1;
+    c.z /= point.size()-1;
     int count=0;
+    srand(time(NULL));
 
-
-    
+    int tick = 0,tick1=0,tick2=0,tick3=0,tick4=0,tick5=0,tick6=0,tick7=0;
     while (!run)
     {
       SDL_Event e;
-      SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+      SDL_SetRenderDrawColor(ren, 0, 0, 0xff, SDL_ALPHA_OPAQUE);
+
       foreground = {fR, fG, fB};
-      text_surf = TTF_RenderUTF8_Blended_Wrapped(font, texts.c_str(), foreground, 22);
+      if(tick==10){
+      texts1.clear();
+      int luck=3*(rand()%22);
+      texts1.push_back(hira.c_str()[luck]);
+      texts1.push_back(hira.c_str()[++luck]);
+      texts1.push_back(hira.c_str()[++luck]);
+      texts1.push_back('\0');
+      tick=0;     
+      }
+      if(tick1==15){
+      int luck=3*(rand()%22);
+      texts[18]=(hira.c_str()[luck]);
+      texts[19]=(hira.c_str()[++luck]);
+      texts[20]=(hira.c_str()[++luck]);
+      tick1=0;     
+      }
+      if(tick2==20){
+      int luck=3*(rand()%22);
+      texts[15]=(hira.c_str()[luck]);
+      texts[16]=(hira.c_str()[++luck]);
+      texts[17]=(hira.c_str()[++luck]);
+      tick2=0;     
+      }
+      if(tick3==25){
+      int luck=3*(rand()%22);
+      texts[12]=(hira.c_str()[luck]);
+      texts[13]=(hira.c_str()[++luck]);
+      texts[14]=(hira.c_str()[++luck]);
+      tick3=0;     
+      }
+      if(tick4==30){
+      int luck=3*(rand()%22);
+      texts[9]=(hira.c_str()[luck]);
+      texts[10]=(hira.c_str()[++luck]);
+      texts[11]=(hira.c_str()[++luck]);
+      tick4=0;     
+      }
+      if(tick5==35){
+      int luck=3*(rand()%22);
+      texts[6]=(hira.c_str()[luck]);
+      texts[7]=(hira.c_str()[++luck]);
+      texts[8]=(hira.c_str()[++luck]);
+      tick5=0;     
+      }
+      if(tick6==40){
+      int luck=3*(rand()%22);
+      texts[3]=(hira.c_str()[luck]);
+      texts[4]=(hira.c_str()[++luck]);
+      texts[5]=(hira.c_str()[++luck]);
+      tick6=0;     
+      }
+      if(tick7==45){
+      int luck=3*(rand()%22);
+      texts[0]=(hira.c_str()[luck]);
+      texts[1]=(hira.c_str()[++luck]);
+      texts[2]=(hira.c_str()[++luck]);
+      tick7=0;     
+      }
+
+      text_surf = TTF_RenderUTF8_Blended_Wrapped(font, (const char*)texts.c_str(), foreground, 22);
+      SDL_SetSurfaceAlphaMod(text_surf,255);
+      
       text = SDL_CreateTextureFromSurface(ren, text_surf);
+      SDL_SetTextureAlphaMod(text,255);
       foreground = {255, 255, 255,255};
       text_surf1 = TTF_RenderUTF8_Blended_Wrapped(font, texts1.c_str(), foreground, 22);
+      SDL_SetSurfaceAlphaMod(text_surf1,255);
       text1 = SDL_CreateTextureFromSurface(ren, text_surf1);
 
+      SDL_SetTextureAlphaMod(text,255);
+      SDL_SetSurfaceBlendMode(buffer,SDL_BLENDMODE_BLEND);
+      SDL_SetSurfaceBlendMode(text_surf,SDL_BLENDMODE_BLEND);
+      SDL_SetSurfaceBlendMode(text_surf1,SDL_BLENDMODE_BLEND);
+      SDL_BlitSurface(text_surf, NULL,buffer , NULL);
+      SDL_BlitSurface(text_surf1, NULL, buffer, NULL);
+
+      SDL_SetTextureBlendMode(text,SDL_BLENDMODE_BLEND);
+      SDL_SetTextureBlendMode(text1,SDL_BLENDMODE_BLEND);
+      SDL_SetRenderDrawBlendMode(ren,SDL_BLENDMODE_BLEND);
 
       for (auto &p : point)
       {
+          
+        p.z += (rand() % 10);
 
-        p.z += (rand() % 10)+1;
         if (p.z > widthW)
         {
           p.z = 0;
@@ -222,6 +209,7 @@ inline void MainMenu::update()
         float sy = (p.z / p.y) * heightW - heightW / 2;
 
         pixel(&points,&dest,text_surf,text_surf1, sx, sy);
+
         
       }
       show(points, dest,text,text1,ren);
@@ -251,10 +239,11 @@ inline void MainMenu::update()
         run = true;
         break;
       }
-
+    tick++;tick1++;tick2++;tick3++;tick4++;tick5++;tick6++;tick7++;
     SDL_FreeSurface(text_surf);
     SDL_FreeSurface(text_surf1);
 
+    
   }
 }
 void MainMenu::exit()
@@ -267,11 +256,13 @@ void MainMenu::exit()
   running=true;
 }
 MainMenu::~MainMenu(){}
-void MainMenu::show(std::vector<SDL_FPoint> points, std::vector<SDL_Rect> dest,SDL_Texture *text,SDL_Texture *text1,SDL_Renderer *renderer)
+void MainMenu::show(std::vector<SDL_FPoint> points, std::vector<SDL_Rect> dest,
+SDL_Texture *text,SDL_Texture *text1,SDL_Renderer *renderer)
 {
-  SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+  SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(ren);
-  SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+  SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
   int count =0;
   for (auto &point : points)
   {
@@ -281,6 +272,7 @@ void MainMenu::show(std::vector<SDL_FPoint> points, std::vector<SDL_Rect> dest,S
     SDL_RenderCopy(ren, text1, NULL, &dest[count++]);
 
   }
+
   SDL_DestroyTexture(text);
   SDL_DestroyTexture(text1);
   SDL_RenderPresent(ren);
@@ -306,7 +298,6 @@ void MainMenu::pixel(std::vector<SDL_FPoint> *points,std::vector<SDL_Rect> *dest
   rect1.x = x;
   rect1.w = text_surf1->w;
   rect1.h = text_surf1->h;
-
   points->push_back(i);
   dest->push_back(rect1);
 }
@@ -410,6 +401,3 @@ void Intro::exit()
   running=true;
 }
 Intro::~Intro() {}
-
-
-
